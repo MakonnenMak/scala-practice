7d37657d25ea678ceaeced989b7187b75b18895f
mmako1 <solmak11@gmail.com>
2020-07-13 22:09:19 -0400

    Added division for Calculator

--- problems/Calculator.scala
+++ problems/Calculator.scala
@@ -20,14 +20,35 @@ object runner extends App {
                 case Success(rightResult) => Success(leftResult + rightResult)
               }
           }
+        case Division(n, d) =>
+          n.eval match {
+            case Failure(message) => Failure(message)
+            case Success(numeratorResult) =>
+              d.eval match {
+                case Failure(reason) => Failure(reason)
+                case Success(denominatorResult) =>
+                  if (denominatorResult == 0)
+                    Failure("Can't divide by 0")
+                  else
+                    Success(numeratorResult / denominatorResult)
+              }
+          }
       }
   }
 
   final case class Addition(left: Expression, right: Expression)
       extends Expression;
+  final case class Division(numerator: Expression, denominator: Expression)
+      extends Expression;
   final case class Number(value: Double) extends Expression;
-  val num1 = Number(10);
-  val num2 = Number(5);
-  val myAdd = Addition(num1, num2);
+  val ten = Number(10);
+  val five = Number(5);
+  val zero = Number(0);
+  val myAdd = Addition(ten, five);
+  val myDivideSuccess = Division(ten, five);
+  val myDivideFailure = Division(ten, zero);
   println(myAdd.eval)
+  println(myDivideSuccess.eval)
+  println(myDivideFailure.eval)
+
 }
