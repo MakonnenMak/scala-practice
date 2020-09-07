e3bb7e334c6ae954904412c7c3d40d4955873f8a
MakonnenMak <solmak11@gmail.com>
2020-09-07 18:16:27 -0400

    6.2 done

--- Questions/MoreSequencePractice.scala
+++ Questions/MoreSequencePractice.scala
@@ -4,4 +4,20 @@ object Runner extends App {
     seq.sortWith((a, b) => a < b).head
   // Given Seq[Int] create a Seq[Int] containing only the unique elements
 
+  def addUnique(seq: Seq[Int], value: Int): Seq[Int] =
+    if (seq contains value)
+      seq
+    else
+      seq :+ (value)
+  def findUnique(seq: Seq[Int]): Seq[Int] =
+    seq.foldLeft(Seq[Int]())((prevSeq, nextVal) => addUnique(prevSeq, nextVal))
+
+  def reverseHelper(seq: Seq[Int], value: Int): Seq[Int] =
+    (value) +: seq
+
+  def reverseWithFold(seq: Seq[Int]): Seq[Int] =
+    seq.foldLeft(Seq[Int]())((prevSeq, nextVal) =>
+      reverseHelper(prevSeq, nextVal)
+    )
+
 }
